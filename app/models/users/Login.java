2b677796c0c2fc5aa71d4bb287af318502f63555
vijay <x00125075@gmail.com>
2016-11-28 16:33:43 +0000

    test1

--- app/models/users/Login.java
+++ app/models/users/Login.java
@@ -4,4 +4,15 @@ package models.users;
  * Created by wdd on 28/11/16.
  */
 public class Login {
+    private String email;
+    private String password;
+
+    public String validate)(){
+        if (User.authenticate(getEmail(), getPassword()) == null) {
+            return " Invalid User OR Password";
+        }
+        return null;
+    }
+    
+
 }
