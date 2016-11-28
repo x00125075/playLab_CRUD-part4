c33264e0ffbeb970567c40d80289952dc7b8bb50
vijay <x00125075@gmail.com>
2016-11-28 15:57:49 +0000

    Details to commit

--- app/models/users.java
+++ app/models/users.java
@@ -27,7 +27,42 @@ public class users extends Model {
 
 
     }
-    public static 
+    public static Finder <String.User> finsd = new Finder<String,Usre>(User.class);
 
+    public static List<users> findall () {
+        return Users.find.all();
 
+    }
+    public static Users authentication(Sting email, Sting password){
+        return find.where().eq("Email", email).eq(password, password).findUnique();
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
